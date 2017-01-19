b317edb68a157728369d2f763781e2210318c57d
helyho <helyho@gmail.com>
2017-01-19 17:44:23 +0800

    init

--- WEBAPP/js/utils.js
+++ WEBAPP/js/utils.js
@@ -32,7 +32,7 @@
     function alertError(e){
         try {
             var errObj = eval("err_" + currentTimeMills() + " = " + e.message);
-            UIkit.modal.alert(errObj.errMsg);
+            UIkit.modal.alert(errObj.errMsg.replaceAll("\\\"","\""));
         }catch(e){
             UIkit.modal.alert(e.message);
         }
@@ -73,6 +73,15 @@
 /**
  * =======================基础类扩展函数=======================
  */
+
+    String.prototype.replaceAll = function (sptr, sptr1){
+        var str = this;
+        while (str.indexOf(sptr) >= 0){
+            str = str.replace(sptr, sptr1);
+        }
+        return str;
+    }
+
     // 对Date的扩展，将 Date 转化为指定格式的String
     // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
     // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
