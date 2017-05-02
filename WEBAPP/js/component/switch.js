1b139f20403fc1bd7008130307bfa038e46720b8
helyho <helyho@gmail.com>
2017-05-02 13:50:52 +0800

    imp: 优化 Service 和 Contianer 的创建,根据选择的 image 自动创建端口和卷的提示

--- WEBAPP/js/component/switch.js
+++ WEBAPP/js/component/switch.js
@@ -1,12 +1,12 @@
 Vue.component('c-switch', {
     template:   "<span>"+
     "<div data-uk-button-radio class='uk-button-group'>"+
-    "      <a href='#' v-for='(value,key) in switchData' " +
+    "      <button type='button' v-for='(value,key) in switchData' " +
     "           class='uk-button' :class='[bindData==value?\"uk-active\":\"\",theme]'"+
-    "           @click='clickOn(key)' >{{key}}</a>"+
+    "           @click='clickOn(key)'>{{key}}</input>"+
     "</div>" +
     "</span>",
-    props: ['bind','theme','switch'],
+    props: ['bind','theme','switch', 'disabled'],
     computed: {
         switchData: function() {
             if (this.switch instanceof Object) {
@@ -22,10 +22,10 @@ Vue.component('c-switch', {
     methods: {
         clickOn: function(index){
             var result = this.switchData[index];
-            if((typeof result) == "string"){
-                result = "\""+result+"\"";
+            if ((typeof result) == "string") {
+                result = "\"" + result + "\"";
             }
-            eval("this.$parent."+this.bind+"="+result);
+            eval("this.$parent." + this.bind + "=" + result);
         }
     }
 })
