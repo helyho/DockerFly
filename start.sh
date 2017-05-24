25a31d101990590ac7405de3b02af8baf2424c3e
helyho <helyho@gmail.com>
2017-05-24 17:00:14 +0800

    add: 增加start.sh启动前编译 java 文件的能力

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
+find . -name "*.java" > source.list
+javac -d classes -classpath ./bin/voovan-framework.jar:./lib/JDocker.jar:./lib/vestful.jar:./lib/sqlite-jdbc-3.16.1.jar @source.list
+rm source.list
+cp classes/main/resources/logger.properties logger.properties
+rm -rf classes/main
 nohup socat tcp4-listen:2735,reuseaddr,fork unix-connect:/var/run/docker.sock >> /dev/null 2>&1 &
 java -jar ./bin/voovan-framework.jar 
