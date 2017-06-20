a1c73b795d7a0cc4ce5bd4686f011dbdc0087408
helyho <helyho@gmail.com>
2017-06-20 16:17:30 +0800

    imp: 优化 start.sh,仅在无法找到 class 目录时进行编译

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@ if [ -d ./src ]; then
 
     if [ ! -d ./classes ]; then
         mkdir classes
-
     fi
 
     if [ ! -d ./classes/org/voovan/dockerfly ]; then
@@ -11,12 +10,12 @@ if [ -d ./src ]; then
         find . -name "*.java" > source.list
         javac -d classes -classpath ./bin/voovan-framework.jar:./lib/JDocker.jar:./lib/vestful.jar:./lib/sqlite-jdbc-3.16.1.jar @source.list
         rm source.list
-
     fi
 
     if [ ! -f ./classes/logger.properties ]; then
-     cp src/main/resources/logger.properties classes/logger.properties
+        cp src/main/resources/logger.properties classes/logger.properties
     fi
 fi
+
 nohup socat tcp4-listen:2735,reuseaddr,fork unix-connect:/var/run/docker.sock >> /dev/null 2>&1 &
 java -jar ./bin/voovan-framework.jar
